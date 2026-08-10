10b6a3ac2ce10599c38152db776fa6ab038a3f4c
Wolf3s <andregui17@outlook.com>
2026-08-10 19:39:38 -0300

    PS2 port
    Co-authored-by: Adrian Siekierka <kontakt@asie.pl>

--- config.sh
+++ config.sh
@@ -32,6 +32,7 @@ usage() {
 	echo "  darwin         Mac OS X Unix-like install"
 	echo "  darwin-devel   Mac OS X running from current dir"
 	echo "  darwin-dist    Mac OS X multiarchitecture .app (see arch/darwin/README.md)"
+	echo " 	ps2 		   Experimental PS2 port"
 	echo "  psp            Experimental PSP port"
 	echo "  gp2x           Experimental GP2X port"
 	echo "  nds            Experimental NDS port"
@@ -1067,6 +1068,27 @@ case "$PLATFORM" in
 
 	echo "Force-disabling runtime icon loading on PSP."
 	ICON="false"
+
+	echo "Force-disabling getaddrinfo, poll, and IPv6 on PSP."
+	GETADDRINFO="false"
+	POLL="false"
+	IPV6="false"
+	;;
+	
+"ps2")
+	echo "Enabling PSP-specific hacks."
+	echo "#define CONFIG_PS2" >> src/config.h
+	echo "BUILD_PS2=1" >> platform.inc
+
+	echo "Force-disabling modular build on PSP."
+	MODULAR="false"
+
+	echo "Force-disabling OpenGL and overlay renderers on PSP."
+	GL="false"
+	OVERLAY="false"
+
+	echo "Force-disabling stack protector on PSP."
+	STACK_PROTECTOR="false"
 
 	echo "Force-disabling getaddrinfo, poll, and IPv6 on PSP."
 	GETADDRINFO="false"
